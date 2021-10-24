067a2156cce36b7546cd845fe0ab3723c2641172
hoangngoctan17 <hoangngoctan17@gmail.com>
2021-10-24 10:24:55 +0700

    simple widget

--- section2_flutter_basics/flutter_complete_guide/lib/main.dart
+++ section2_flutter_basics/flutter_complete_guide/lib/main.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'));
+    var questions = [
+      'What is your favorite color?',
+      'What is your favorite animal'
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text('The Question!'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: null,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
