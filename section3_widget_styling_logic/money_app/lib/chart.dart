52b66c0fcd9fe1fbeea80914f3917fdb18ca36cd
Tan2NT <hoangngoctan17@gmail.com>
2021-10-31 09:55:27 +0700

    Section 4 - draw a chart

---
+++ section3_widget_styling_logic/money_app/lib/chart.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:money_app/chart_bar.dart';
+import 'package:money_app/transaction.dart';
+import 'package:intl/intl.dart';
+
+import './transaction.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      var weekDay = DateTime.now().subtract(Duration(days: index));
+      double totalSum = 0;
+      for (int i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekDay.day &&
+            recentTransactions[i].date.month == weekDay.month &&
+            recentTransactions[i].date.year == weekDay.year) {
+          totalSum += recentTransactions[i].amount;
+        }
+      }
+
+      return {'day': DateFormat.E().format(weekDay), 'amount': totalSum};
+    });
+  }
+
+  double get totalSpending {
+    return groupedTransactionValues.fold(0.0, (sum, item) {
+      return sum + double.parse(item['amount'].toString());
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        elevation: 6,
+        margin: EdgeInsets.all(10),
+        child: Container(
+          padding: EdgeInsets.all(10),
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: groupedTransactionValues.map((data) {
+                return Flexible(
+                    flex: 1,
+                    fit: FlexFit.tight,
+                    child: ChartBar(
+                        data['day'].toString(),
+                        double.parse(data['amount'].toString()),
+                        double.parse(data['amount'].toString()) /
+                            totalSpending));
+              }).toList()),
+        ));
+  }
+}
--- section3_widget_styling_logic/money_app/lib/user_transaction.dart
+++ section3_widget_styling_logic/money_app/lib/user_transaction.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import './transaction.dart';
 import './transaction_input.dart';
 import './transaction_list.dart';
+import './chart.dart';
 
 class UserTransaction extends StatefulWidget {
   const UserTransaction({Key? key}) : super(key: key);
@@ -13,14 +14,14 @@ class UserTransaction extends StatefulWidget {
 
 class _UserTransactionState extends State<UserTransaction> {
   final List<Transaction> transactions = [
-    // Transaction(DateTime.now().millisecondsSinceEpoch.toString(), "Buy a ball",
-    //     23.45, DateTime.now()),
-    // Transaction(DateTime.now().millisecondsSinceEpoch.toString(), "Living fee",
-    //     69.99, DateTime.now()),
-    // Transaction(DateTime.now().millisecondsSinceEpoch.toString(), "Chicken dry",
-    //     200.00, DateTime.now()),
-    // Transaction(DateTime.now().millisecondsSinceEpoch.toString(), "Test 1",
-    //     24.00, DateTime.now()),
+    Transaction(DateTime.now().millisecondsSinceEpoch.toString(), "Buy a ball",
+        23.45, DateTime.now()),
+    Transaction(DateTime.now().millisecondsSinceEpoch.toString(), "Living fee",
+        69.99, DateTime.now()),
+    Transaction(DateTime.now().millisecondsSinceEpoch.toString(), "Chicken dry",
+        200.00, DateTime.now()),
+    Transaction(DateTime.now().millisecondsSinceEpoch.toString(), "Test 1",
+        24.00, DateTime.now()),
     // Transaction(DateTime.now().millisecondsSinceEpoch.toString(), "Test 2",
     //     46.00, DateTime.now()),
     // Transaction(DateTime.now().millisecondsSinceEpoch.toString(), "Test 3",
@@ -50,6 +51,15 @@ class _UserTransactionState extends State<UserTransaction> {
     });
   }
 
+  List<Transaction> get _recentTransactions {
+    var myList = [0, 2, 4, 6, 8, 2, 7];
+    myList.where((item) => item > 5).toList();
+    return transactions
+        .where(
+            (tx) => tx.date.isAfter(DateTime.now().subtract(Duration(days: 7))))
+        .toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -66,6 +76,7 @@ class _UserTransactionState extends State<UserTransaction> {
       ),
       body: Column(
         children: [
+          Chart(_recentTransactions),
           TransactionList(transactions),
         ],
         // ),
