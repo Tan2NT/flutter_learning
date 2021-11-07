1025d6aaa2eed4a0f881199b538a67b6628e4aca
Tan2NT <hoangngoctan17@gmail.com>
2021-11-07 23:15:38 +0700

    section 7 - add tab bar

--- section7_navigation/meals_app/lib/main.dart
+++ section7_navigation/meals_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/screens/category_meals_screen.dart';
 import 'package:meals_app/screens/meal_detail_screen.dart';
+import 'package:meals_app/screens/tab_screen.dart';
 
 import 'screens/categories_screen.dart';
 
@@ -30,9 +31,9 @@ class MyApp extends StatelessWidget {
                     fontWeight: FontWeight.bold),
               )),
       // home: CategoriesScreen(),
-      initialRoute: CategoriesScreen.routeName,
+      initialRoute: '/',
       routes: {
-        CategoriesScreen.routeName: (ctx) => CategoriesScreen(),
+        '/': (ctx) => TabScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen()
       },
--- section7_navigation/meals_app/lib/screens/categories_screen.dart
+++ section7_navigation/meals_app/lib/screens/categories_screen.dart
@@ -10,21 +10,18 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Meals app')),
-      body: GridView(
-          padding: const EdgeInsets.all(25),
-          children: [
-            ...DUMMY_CATEGORIES
-                .map((cateData) =>
-                    CategoryItem(cateData.id, cateData.title, cateData.color))
-                .toList()
-          ],
-          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: 200,
-              childAspectRatio: 3 / 2,
-              crossAxisSpacing: 20,
-              mainAxisSpacing: 20)),
-    );
+    return GridView(
+        padding: const EdgeInsets.all(25),
+        children: [
+          ...DUMMY_CATEGORIES
+              .map((cateData) =>
+                  CategoryItem(cateData.id, cateData.title, cateData.color))
+              .toList()
+        ],
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20));
   }
 }
