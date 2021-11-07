a535c8e2f1eb8f65e2ac03196516bb8783af3dd1
Tan2NT <hoangngoctan17@gmail.com>
2021-11-07 12:04:18 +0700

    section 7 - finish the meal list item

--- section7_navigation/meals_app/lib/widgets/meal_item.dart
+++ section7_navigation/meals_app/lib/widgets/meal_item.dart
@@ -15,6 +15,32 @@ class MealItem extends StatelessWidget {
       this.complexity = Complexity.Simple,
       this.affordability = Affordability.Affordable});
 
+  String getComplexityText() {
+    switch (complexity) {
+      case Complexity.Simple:
+        return 'Simple';
+      case Complexity.Challenging:
+        return 'Challenging';
+      case Complexity.Hard:
+        return 'Hard';
+      default:
+        return 'Unknown';
+    }
+  }
+
+  String getAffordabilityText() {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return 'Affordable';
+      case Affordability.Pricey:
+        return 'Pricey';
+      case Affordability.Luxurious:
+        return 'Luxurious';
+      default:
+        return 'Unknown';
+    }
+  }
+
   void selectMeal() {}
 
   @override
@@ -34,7 +60,58 @@ class MealItem extends StatelessWidget {
                       topRight: Radius.circular(15)),
                   child: Image.network(imageUrl,
                       height: 250, width: double.infinity, fit: BoxFit.cover)),
-            ])
+              Positioned(
+                bottom: 20,
+                right: 10,
+                child: Container(
+                    width: 220,
+                    color: Colors.black54,
+                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    )),
+              )
+            ]),
+            Padding(
+                padding: EdgeInsets.all(20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(Icons.schedule),
+                        SizedBox(
+                          width: 6,
+                        ),
+                        Text('${duration} min'),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.work),
+                        SizedBox(
+                          width: 6,
+                        ),
+                        Text('${getComplexityText()}'),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.attach_money),
+                        SizedBox(
+                          width: 6,
+                        ),
+                        Text('${getAffordabilityText()}'),
+                      ],
+                    ),
+                  ],
+                ))
           ],
         ),
       ),
