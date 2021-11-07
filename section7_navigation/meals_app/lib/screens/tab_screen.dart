b57ffe5ca52bf1188daf13784cbbcb2fe71134eb
Tan2NT <hoangngoctan17@gmail.com>
2021-11-08 00:04:50 +0700

    section 7 - adding a custom drawer

--- section7_navigation/meals_app/lib/screens/tab_screen.dart
+++ section7_navigation/meals_app/lib/screens/tab_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/screens/categories_screen.dart';
 import 'package:meals_app/screens/favorites_screen.dart';
+import 'package:meals_app/widgets/main_drawer.dart';
 import 'categories_screen.dart';
 
 class TabScreen extends StatefulWidget {
@@ -27,6 +28,7 @@ class _TabScreenState extends State<TabScreen> {
       appBar: AppBar(
         title: Text(_pages[_selectedPageIndex]['title'] as String),
       ),
+      drawer: MainDrawer(),
       body: _pages[_selectedPageIndex]['page'] as Widget,
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
