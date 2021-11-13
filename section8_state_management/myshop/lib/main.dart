6fe36509dbce8652de8cf528521b808f0c41684d
Tan2NT <hoangngoctan17@gmail.com>
2021-11-14 00:35:09 +0700

    state management - working with Provider and listener

--- section8_state_management/myshop/lib/main.dart
+++ section8_state_management/myshop/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:myshop/screens/product_detail_screen.dart';
+import 'package:myshop/providers/products.dart';
+import 'package:provider/provider.dart';
 
 import 'screens/product_overview_screen.dart';
+import 'providers/products.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,17 +16,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato',
+        ),
+        home: ProductOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
       ),
-      home: ProductOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-      },
     );
   }
 }
