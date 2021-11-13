b6ebbfa8abbce3b502bdf9c5efaf1ce9dc2d303d
Tan2NT <hoangngoctan17@gmail.com>
2021-11-13 23:06:32 +0700

    section 8 - initia Myshop

---
+++ section8_state_management/myshop/lib/widgets/product_item.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  ProductItem(this.id, this.title, this.imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+      child: Image.network(imageUrl, fit: BoxFit.cover),
+      footer: GridTileBar(
+        backgroundColor: Colors.black54,
+        leading: IconButton(onPressed: () {}, icon: Icon(Icons.favorite)),
+        trailing: IconButton(onPressed: () {}, icon: Icon(Icons.card_travel)),
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
