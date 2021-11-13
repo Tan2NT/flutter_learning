fe693ffcb932c2c7e41af95c4c176f04dc7cbf0a
Tan2NT <hoangngoctan17@gmail.com>
2021-11-13 23:17:19 +0700

    state management - styling and theming the app

--- section8_state_management/myshop/lib/widgets/product_item.dart
+++ section8_state_management/myshop/lib/widgets/product_item.dart
@@ -9,15 +9,25 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(imageUrl, fit: BoxFit.cover),
-      footer: GridTileBar(
-        backgroundColor: Colors.black54,
-        leading: IconButton(onPressed: () {}, icon: Icon(Icons.favorite)),
-        trailing: IconButton(onPressed: () {}, icon: Icon(Icons.card_travel)),
-        title: Text(
-          title,
-          textAlign: TextAlign.center,
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: Image.network(imageUrl, fit: BoxFit.cover),
+        footer: GridTileBar(
+          backgroundColor: Colors.black,
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.favorite),
+            color: Theme.of(context).accentColor,
+          ),
+          trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.card_travel),
+              color: Theme.of(context).accentColor),
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
         ),
       ),
     );
