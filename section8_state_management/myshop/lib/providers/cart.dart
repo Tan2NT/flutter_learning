86bd4a6d1d482d6e48f7a7f430f322e186f84d16
Tan Hoang <tan.hoang@smartdev.com>
2021-11-19 17:58:02 +0700

    state management - adding CartItem

---
+++ section8_state_management/myshop/lib/providers/cart.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/foundation.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CartItem(this.id, this.title, this.quantity, this.price);
+}
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items = Map();
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  void addItem(String productId, String title, double price) {
+    if (_items.containsKey(productId)) {
+      _items.update(
+          productId,
+          (existingCartItem) => CartItem(
+              existingCartItem.id,
+              existingCartItem.title,
+              existingCartItem.quantity + 1,
+              existingCartItem.price));
+    } else {
+      _items.putIfAbsent(productId,
+          () => CartItem(DateTime.now().toString(), title, 1, price));
+    }
+  }
+}
