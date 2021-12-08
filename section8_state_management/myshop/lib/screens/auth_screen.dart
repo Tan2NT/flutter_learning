9440bfdb467ea1a1074edb1ed3c915cd64476bfd
Tan Hoang <tan.hoang@smartdev.com>
2021-12-09 00:15:39 +0700

    animation - add animation when switch login mode

--- section8_state_management/myshop/lib/screens/auth_screen.dart
+++ section8_state_management/myshop/lib/screens/auth_screen.dart
@@ -90,12 +90,34 @@ class AuthCard extends StatefulWidget {
   }
 }
 
-class _AuthCardState extends State<AuthCard> {
+class _AuthCardState extends State<AuthCard>
+    with SingleTickerProviderStateMixin {
   final GlobalKey<FormState> _formKey = GlobalKey();
   AuthMode _authMode = AuthMode.Login;
   Map<String, String> _authData = {'email': '', 'password': ''};
   bool _isLoading = false;
   final _passwordController = TextEditingController();
+  AnimationController? _controller;
+  Animation<Size>? _heightAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 500));
+    _heightAnimation = Tween<Size>(
+            begin: Size(double.infinity, 260.0),
+            end: Size(double.infinity, 320))
+        .animate(
+            CurvedAnimation(parent: _controller!, curve: Curves.fastOutSlowIn));
+    _heightAnimation!.addListener(() => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    _controller!.dispose();
+    super.dispose();
+  }
 
   void _showErrorDialog(String message) {
     showDialog(
@@ -158,10 +180,12 @@ class _AuthCardState extends State<AuthCard> {
       setState(() {
         _authMode = AuthMode.Signup;
       });
+      _controller!.forward();
     } else {
       setState(() {
         _authMode = AuthMode.Login;
       });
+      _controller!.reverse();
     }
   }
 
@@ -172,10 +196,11 @@ class _AuthCardState extends State<AuthCard> {
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
       elevation: 8.0,
       child: Container(
-        height: _authMode == AuthMode.Signup ? 320 : 260,
+        // height: _authMode == AuthMode.Signup ? 320 : 260,
+        height: _heightAnimation!.value.height,
         constraints: BoxConstraints(
-          minHeight: _authMode == AuthMode.Signup ? 320 : 260,
-        ),
+            // minHeight: _authMode == AuthMode.Signup ? 320 : 260,
+            minHeight: _heightAnimation!.value.height),
         width: deviceSize.width * 0.75,
         padding: EdgeInsets.all(16.0),
         child: Form(
@@ -221,6 +246,9 @@ class _AuthCardState extends State<AuthCard> {
                                 }
                               }
                             : null),
+                  Divider(
+                    height: 20,
+                  ),
                   if (_isLoading)
                     CircularProgressIndicator()
                   else
