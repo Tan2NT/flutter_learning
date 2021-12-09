af688415376188476f6bf82066c187dcda74dc7b
Tan Hoang <tan.hoang@smartdev.com>
2021-12-09 23:16:55 +0700

    animation - working with Sliver (same as motion layout in android)

--- section8_state_management/myshop/lib/screens/product_detail_screen.dart
+++ section8_state_management/myshop/lib/screens/product_detail_screen.dart
@@ -15,34 +15,46 @@ class ProductDetailScreen extends StatelessWidget {
     final productId = ModalRoute.of(context)?.settings.arguments as String;
     final loadedProduct = Provider.of<Products>(context).findById(productId);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(loadedProduct.title),
-      ),
-      body: Column(
-        children: [
-          Container(
-            height: 300,
-            width: double.infinity,
-            child: Hero(
-              tag: loadedProduct.id,
-              child: Image.network(
-                loadedProduct.imageUrl,
-                fit: BoxFit.cover,
+      // appBar: AppBar(
+      //   title: Text(loadedProduct.title),
+      // ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 300,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(loadedProduct.title),
+              background: Hero(
+                tag: loadedProduct.id,
+                child: Image.network(
+                  loadedProduct.imageUrl,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
           ),
-          SizedBox(
-            height: 10,
-          ),
-          Text('\$${loadedProduct.price}',
-              style: TextStyle(color: Colors.green, fontSize: 20)),
-          SizedBox(
-            height: 10,
-          ),
-          Container(
-              padding: EdgeInsets.all(15),
-              child:
-                  Text(loadedProduct.description, textAlign: TextAlign.center))
+          SliverList(
+              delegate: SliverChildListDelegate([
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              '\$${loadedProduct.price}',
+              style: TextStyle(color: Colors.green, fontSize: 20),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+                padding: EdgeInsets.all(15),
+                child: Text(loadedProduct.description,
+                    textAlign: TextAlign.center)),
+            SizedBox(
+              height: 800,
+            ),
+          ]))
         ],
       ),
     );
