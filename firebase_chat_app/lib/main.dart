5451a80f5a02af55db7eb46b926ffd949ce79f12
Tan Hoang <tan.hoang@smartdev.com>
2021-12-11 16:44:19 +0700

    firebase - using firebase authentication

--- firebase_chat_app/lib/main.dart
+++ firebase_chat_app/lib/main.dart
@@ -2,8 +2,10 @@ import 'package:firebase_chat_app/screens/chat_screen.dart';
 import 'package:firebase_chat_app/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import './screens/chat_screen.dart';
+import './screens/auth_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,25 +20,37 @@ class MyApp extends StatelessWidget {
     final Future<FirebaseApp> _initialization = Firebase.initializeApp();
     return FutureBuilder(
         future: _initialization,
-        builder: (ctx, appSapshot) {
+        builder: (ctx, appSnapshot) {
           return MaterialApp(
-              title: 'FlutterChat',
-              theme: ThemeData(
-                primarySwatch: Colors.pink,
-                backgroundColor: Colors.pink,
-                accentColor: Colors.deepPurple,
-                accentColorBrightness: Brightness.dark,
-                buttonTheme: ButtonTheme.of(context).copyWith(
-                  buttonColor: Colors.pink,
-                  textTheme: ButtonTextTheme.primary,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
+            title: 'FlutterChat',
+            theme: ThemeData(
+              primarySwatch: Colors.pink,
+              backgroundColor: Colors.pink,
+              accentColor: Colors.deepPurple,
+              accentColorBrightness: Brightness.dark,
+              buttonTheme: ButtonTheme.of(context).copyWith(
+                buttonColor: Colors.pink,
+                textTheme: ButtonTextTheme.primary,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
                 ),
               ),
-              home: appSapshot.connectionState != ConnectionState.done
-                  ? SplashScreen()
-                  : ChatScreen());
+            ),
+            home: appSnapshot.connectionState != ConnectionState.done
+                ? SplashScreen()
+                : StreamBuilder(
+                    stream: FirebaseAuth.instance.authStateChanges(),
+                    builder: (ctx, userSnapshot) {
+                      if (userSnapshot.connectionState ==
+                          ConnectionState.waiting) {
+                        return SplashScreen();
+                      }
+                      if (userSnapshot.hasData) {
+                        return ChatScreen();
+                      }
+                      return AuthScreen();
+                    }),
+          );
         });
   }
 }
