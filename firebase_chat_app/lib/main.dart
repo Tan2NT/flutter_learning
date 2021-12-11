758c52154c0b60ece2d1e67dabd79ddcd1395b05
Tan Hoang <tan.hoang@smartdev.com>
2021-12-11 10:06:39 +0700

    firebase init Firebase app correctly, geting stream from Firebase cloud storage & put data into it

--- firebase_chat_app/lib/main.dart
+++ firebase_chat_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_chat_app/screens/chat_screen.dart';
+import 'package:firebase_chat_app/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
@@ -14,12 +15,28 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    Firebase.initializeApp();
-    return MaterialApp(
-        title: 'FlutterChat',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: ChatScreen());
+    final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+    return FutureBuilder(
+        future: _initialization,
+        builder: (ctx, appSapshot) {
+          return MaterialApp(
+              title: 'FlutterChat',
+              theme: ThemeData(
+                primarySwatch: Colors.pink,
+                backgroundColor: Colors.pink,
+                accentColor: Colors.deepPurple,
+                accentColorBrightness: Brightness.dark,
+                buttonTheme: ButtonTheme.of(context).copyWith(
+                  buttonColor: Colors.pink,
+                  textTheme: ButtonTextTheme.primary,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+              home: appSapshot.connectionState != ConnectionState.done
+                  ? SplashScreen()
+                  : ChatScreen());
+        });
   }
 }
--- firebase_chat_app/lib/screens/chat_screen.dart
+++ firebase_chat_app/lib/screens/chat_screen.dart
@@ -5,21 +5,29 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (ctx, index) => Container(
-                padding: EdgeInsets.all(8),
-                child: Text('This works'),
-              )),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('chats/0zf2EGDgh3qrUdKU0pFN/messages')
+            .snapshots(),
+        builder: (ctx, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+          return ListView.builder(
+              itemCount: streamSnapshot.data?.docs.length,
+              itemBuilder: (ctx, index) => Container(
+                    padding: EdgeInsets.all(8),
+                    child: Text(streamSnapshot.data?.docs[index]['text']),
+                  ));
+        },
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          await FirebaseFirestore.instance
+        onPressed: () {
+          FirebaseFirestore.instance
               .collection('chats/0zf2EGDgh3qrUdKU0pFN/messages')
-              .snapshots()
-              .listen((data) {
-            data.docs.forEach((element) {
-              print(element['text']);
-            });
+              .add({
+            'text':
+                'This was added by clicking the button! ${DateTime.now().toIso8601String()}'
           });
         },
         child: Icon(Icons.add),
